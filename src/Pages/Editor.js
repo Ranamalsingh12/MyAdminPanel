ae3f77891e3fcd95cef8683bf7f20c6f2dc09550
ranamalsingh12 <ranams99911@gmail.com>
2022-07-08 13:13:08 +0530

    editor

--- src/Pages/Editor.js
+++ src/Pages/Editor.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+
+import { Header } from '../components';
+import { EditorData } from '../data/dummy';
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className="m-2 md:md-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+        <Header category="App" title="Kanban" />
+        <RichTextEditorComponent>
+          <EditorData />
+          <Inject services={[HtmlEditor, Toolbar, Image , Link, QuickToolbar]} />
+        </RichTextEditorComponent>
+    </div>
   )
 }
 
